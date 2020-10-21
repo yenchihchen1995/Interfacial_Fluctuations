b3c2286a1250a55a061331bfc9ed545de2b5d869
yenchihchen1995 <yenchihchen1995@users.noreply.github.com>
2020-10-21 11:15:28 +0800

    variables

---
+++ Interfacial_Fluctuations/variables.py
@@ -0,0 +1,13 @@
+# Variables 
+import numpy as np
+
+
+data_file = '/Volumes/Louis/LouisEP/Data_Collection/epsilon_0.25/Experiment/experiment.pickle'
+
+initial_frame = 400
+
+everyframe = 10
+
+average_interface_number = 10
+
+bins = np.linspace(0,40,20)
