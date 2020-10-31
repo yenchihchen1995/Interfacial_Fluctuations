d11dba7f1c370d2c2d5cf56a2ec246a71de18b3a
yenchihchen1995 <yenchihchen1995@users.noreply.github.com>
2020-11-01 04:07:44 +0800

    variables.py

--- Interfacial_Fluctuations/variables.py
+++ Interfacial_Fluctuations/variables.py
@@ -4,10 +4,12 @@ import numpy as np
 
 data_file = 'file_location'
 
-initial_frame = 400
+initial_frame = 650
 
-everyframe = 10
+everyframe = 100
 
 average_interface_number = 10
 
 bins = np.linspace(0,40,20)
+
+curvature_cutoff = 0.075
